2663f076226afb7a95b1743e0317b5358aad34c6
jdesmond91 <jonathandesmond91@gmail.com>
2017-01-18 22:32:44 -0500

    Added warnings for automapper

--- PRJ666App/App_Start/AutoMapperConfig.cs
+++ PRJ666App/App_Start/AutoMapperConfig.cs
@@ -10,20 +10,19 @@ namespace PRJ666App.App_Start
     {
         public static void RegisterMappings()
         {
-          
-            Mapper.CreateMap<Controllers.ScenarioAdd, Models.Scenario>();
+            // Disable AutoMapper v4.2.x warnings
+            #pragma warning disable CS0618
 
+            Mapper.CreateMap<Controllers.ScenarioAdd, Models.Scenario>();
             Mapper.CreateMap<Models.Scenario, Controllers.ScenarioBase>();
 
             Mapper.CreateMap<Controllers.SectionAdd, Models.Section>();
-
             Mapper.CreateMap<Models.Section, Controllers.SectionBase>();
 
             Mapper.CreateMap<Controllers.QuestionAdd, Models.Question>();
-
             Mapper.CreateMap<Models.Question, Controllers.QuestionBase>();
 
-
+            #pragma warning restore CS0618
         }
     }
 }
