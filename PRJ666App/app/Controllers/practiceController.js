945718faa8e3c9fd960d00d218dec4af8adb2a62
jdesmond91 <jonathandesmond91@gmail.com>
2017-02-12 03:38:44 -0500

    Added condition to test if top string similarity result was 0.3 or less. Also added comments, changed variable names and refactored code slightly.

--- PRJ666App/app/Controllers/practiceController.js
+++ PRJ666App/app/Controllers/practiceController.js
@@ -1,29 +1,38 @@
 ﻿angular.module("nursingApp").controller("practiceController", ["$scope", "$routeParams", "$http", "$q", "scenarioService", "semanticService", practiceController]);
 function practiceController($scope, $routeParams, $http, $q, scenarioService, semanticService) {
-    $scope.Scenarios = [];
-    var scenarioId = $routeParams.scenario_id;
+
+    var scenarioId = $routeParams.scenario_id; //retrieve id from scenario selected from dropdown
     $scope.status;
+    
+    /* variables to hold the scenario, the scenario's section, the questions for each section and 
+    the question typed by the student */
     $scope.scenario = [];
     $scope.allSections = [];
-    $scope.QuestionsSection2 = [];
+    $scope.sectionQuestions = [];
     $scope.studentQuestion = "";
-    $scope.answer = "";
-    $scope.possibleQuestions = [];
-    $scope.QuestionsSection2Desc = [];
+
+    $scope.sectionStartIndex = 1; //starting index, for now set to 1 until we remove the procedures
+    $scope.answer = ""; //the answer we will return to the student
+    $scope.possibleQuestions = []; //stores the top 3 results from the initial pass through string similarity
+
+    //$scope.sectionQuestionDesc = [];
+    //$scope.continueLoop = true;
+
     $scope.lastTry = [];
-    $scope.continueLoop = true;
-    $scope.sectionStartIndex = 1;
+    
 
     //console.log("Scenario Id: " + scenarioId);
 
+    //function first retrieves all of the sections associated with a scenario, then retrieves all the questions associated with that section
+    //for now, we will retrieve starting at index 1, since procedures have not been removed
     $scope.getScenariosByIdWithAll = function () {
-        var scenarioResult2 = scenarioService.getScenarioByIdWithAll(scenarioId);
-        scenarioResult2.then(function (response) {
+        var scenarioResult = scenarioService.getScenarioByIdWithAll(scenarioId);
+        scenarioResult.then(function (response) {
             $scope.scenario = response.data;
             //console.log($scope.scenario);
             $scope.allSections = $scope.scenario.Sections;
             //console.log($scope.allSections);
-            $scope.QuestionsSection2 = $scope.allSections[$scope.sectionStartIndex].Questions;
+            $scope.sectionQuestions = $scope.allSections[$scope.sectionStartIndex].Questions;
         }, function (error) {
             $scope.status = 'Unable to load question data: ' + error.message;
         });        
@@ -36,12 +45,12 @@ function practiceController($scope, $routeParams, $http, $q, scenarioService, se
     /*$scope.getAnswer = function () {
         console.log("getAnswer");
 
-        for (var x = 0; x < $scope.QuestionsSection2.length; x++) {
+        for (var x = 0; x < $scope.sectionQuestions.length; x++) {
             var isMatch = "";
             var countMatches = 0;
 
-            for (var y = 0; y < $scope.QuestionsSection2[x].Keywords.length; y++) {
-                var keyword = $scope.QuestionsSection2[x].Keywords[y].Description;
+            for (var y = 0; y < $scope.sectionQuestions[x].Keywords.length; y++) {
+                var keyword = $scope.sectionQuestions[x].Keywords[y].Description;
 
                 var regexKeyword = new RegExp("\\b" + keyword + "[a-zA-Z]*\\b", "gi"); // global match
 
@@ -52,15 +61,20 @@ function practiceController($scope, $routeParams, $http, $q, scenarioService, se
                 }
             } // close Inner For
 
-            var percentMatch = $scope.QuestionsSection2[x].Keywords.length * 0.8;
+            var percentMatch = $scope.sectionQuestions[x].Keywords.length * 0.8;
             if (Math.floor(countMatches * 0.8) >= Math.floor(percentMatch)) {
-                console.log("possible candidates: " + $scope.QuestionsSection2[x].Description);
-                $scope.answer = $scope.QuestionsSection2[x].Answer;
+                console.log("possible candidates: " + $scope.sectionQuestions[x].Description);
+                $scope.answer = $scope.sectionQuestions[x].Answer;
             }
         } // close Outer For
     }; */
     
 
+    /* Once the user inputs their question and presses the Ask button, the following parse function is called.
+       The function will first attempt to retrieve the 3 best matches from String Similarity by calling the getSimilarity function.
+        If the highest match among those results has a score of 0.3 or less, the API will not be called as the scores are already too low.
+        If the API function is called, then we will return the highest scoring result, FOR NOW
+    */
     $scope.parse = function () {
         console.log("Entering parse\n");
 
@@ -68,14 +82,15 @@ function practiceController($scope, $routeParams, $http, $q, scenarioService, se
 
         $scope.compareAPI = [];
 
-        if (similarityArray[0].similar >= 0.8) {
-            $scope.answer = similarityArray[0].answer;
-        }
-       //else {
+        //if the highest match from string similarity is 0.3 or less, don't bother going to API
+        if (similarityArray[0].similar <= 0.3) {
+            $scope.answer = "Please try asking another question!";
+        } else {
             var max = 0;
             var realAnswer = "";            
             for (var i = 0; i < $scope.possibleQuestions.length; i++) {
-                var api = compareAPI($scope.possibleQuestions[i].question, $scope.possibleQuestions[i].answer, function (score, question, answer) {                    
+                var api = compareAPI($scope.possibleQuestions[i].question, $scope.possibleQuestions[i].answer,
+                function (score, question, answer) {
                     $scope.compareAPI = score.average;
                     console.log($scope.compareAPI);
                     console.log(question);
@@ -89,10 +104,11 @@ function practiceController($scope, $routeParams, $http, $q, scenarioService, se
                   
                 });
             }
-        //}
+        }
     };
 
 
+    //calls the RxNLP API 
     function compareAPI (possibleQuestion, possibleAnswer, fn) {
         console.log("compare API");
     
@@ -106,16 +122,17 @@ function practiceController($scope, $routeParams, $http, $q, scenarioService, se
       
     }
 
+    //calls String Similarity node package to retrieve the top 3 matched questions
     function getSimilarity () {
 
         var stringSimilarity = require('string-similarity');
         var similarityArray = [];
       
         //make an array of object to associate each similarity result with its question
-        for (var i = 0; i < $scope.QuestionsSection2.length; i++) {
-            var stringSim = stringSimilarity.compareTwoStrings($scope.studentQuestion, $scope.QuestionsSection2[i].Description);
-            similarityArray.push({ similar: stringSim, question: $scope.QuestionsSection2[i].Description, 
-                answer: $scope.QuestionsSection2[i].Answer});                
+        for (var i = 0; i < $scope.sectionQuestions.length; i++) {
+            var stringSim = stringSimilarity.compareTwoStrings($scope.studentQuestion, $scope.sectionQuestions[i].Description);
+            similarityArray.push({ similar: stringSim, question: $scope.sectionQuestions[i].Description, 
+                answer: $scope.sectionQuestions[i].Answer});                
         }
 
         //sort the array based on similarity result to get the 3 largest
@@ -136,31 +153,32 @@ function practiceController($scope, $routeParams, $http, $q, scenarioService, se
         return similarityArray;
     }
 
+    // DOM MANIPULATION SECTION ****************************************************************************************//
+
+    $("#question").click(function () {
+        this.value = '';
+    });
+
+    //TEMPORARY: user presses the previous section button which will update the section index and retrieve the previous set of questions
     $scope.start = function () {
         //if (!$scope.continueLoop) return;
         //loopStep();
         //setTimeout($scope.startLoop, 1000);
         --$scope.sectionStartIndex;
-        $scope.QuestionsSection2 = $scope.allSections[$scope.sectionStartIndex].Questions;
+        $scope.sectionQuestions = $scope.allSections[$scope.sectionStartIndex].Questions;
         console.log($scope.sectionStartIndex);
     }
 
+    //user presses the next section button which will update the section index and retrieve the next set of questions
     $scope.stop = function () {
         ++$scope.sectionStartIndex;
-        $scope.QuestionsSection2 = $scope.allSections[$scope.sectionStartIndex].Questions;
+        $scope.sectionQuestions = $scope.allSections[$scope.sectionStartIndex].Questions;
         console.log($scope.sectionStartIndex);
     }
 
 
-    function loopStep() {
-        console.log("test loop");
-    }
-     
-    // DOM MANIPULATION SECTION ****************************************************************************************//
-
-    $("#question").click(function () {
-        this.value = '';
-        $scope.answer = "";
-    });
+    //function loopStep() {
+    //    console.log("test loop");
+    //}
 
 } // close Module
